03a827da77d8148ef8ac920efa3a1bcf4191641b
zuzu <dlwnghks6821@naver.com>
2022-02-05 13:13:21 +0900

    # 검색기능 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button,Table,Dropdown} from "react-bootstrap";
-import { Component, useEffect, useState } from 'react';
+import {Button,Table,InputGroup,FormControl} from "react-bootstrap";
+import { useState } from 'react';
 import axios from 'axios';
 
 
 axios.defaults.withCredentials = true;
 
 const App = () =>{
+  const [searchSongs, setSearchSongs] = useState('');
   const [songs, setSongs] = useState([]);
   const [Selected, setSelected] = useState("");
+  const searchByName = (e) => {
+    setSearchSongs(e.target.value);
+  }
   const getSongs = () =>{
      axios({
       method:"GET",
       url:`http://localhost:3000/crawlling/?genre=${Selected}`
     }).then((res)=>{
-
-      setSongs(res.data)
+      console.log(searchSongs)
+      let search = new RegExp(searchSongs , 'i'); 
+      let keyWord = res.data.filter(item => search.test(item));
+      console.log(keyWord);
+      if(keyWord.length === 0){
+        alert('검색결과가 없습니다.');
+        return false;
+      }
+      setSongs(keyWord)
     })
   }
 const handleSelect = (e) => {
@@ -66,19 +77,27 @@ const SelectBox = () => {
   }
   return(
     <div className="App">
-      <h1> 태고의달인 크롤링</h1>
 
-      <Button onClick={getSongs}>
+      <h1> 태고의달인 크롤링</h1>
+  <div className="formArea">
+  <InputGroup className="mb-3">
+    <FormControl
+      
+      placeholder="곡이름을 검색하세요"
+      onChange={searchByName}
+      aria-describedby="basic-addon1"
+    />
+     <Button onClick={getSongs}>
         곡 가져오기 
       </Button>
       <SelectBox></SelectBox>
+  </InputGroup>
+  </div>
+     
 
       <RenderingSongList></RenderingSongList>
     </div>
   )
-
-  
- 
 }
 
 export default App;
